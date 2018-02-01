17ab8041d4b0838f1c5bbf57d95434a951af0cd4
Paul Savala <paulsavala@gmail.com>
2018-01-31 17:46:09 -0800

    Implemented toy SQLite database

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -5,6 +6,16 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    runs = db.relationship('Run', backref='athlete', lazy='dynamic')
 
     def __repr__(self):
-        return '<User {}'.format(self.username)
+        return '<User {}>'.format(self.username)
+
+class Run(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Run {}>'.format(self.name)

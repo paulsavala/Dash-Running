4977d7762ce80b58d79f9aa8672edea7c7d0579b
Paul Savala <paulsavala@gmail.com>
2018-02-03 15:00:02 -0800

    Setup new user registration framework

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,35 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, \
+                                DataRequired, Email, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), \
+                                                        EqualTo('password')])
+    submit = SubmitField('Register')
+
+    # Methods named "validate_<field_name>" are automatically invoked by Flask
+    # when a field of type "field_name" is run. So since we have both a
+    # "username" field and an "email" field (in the RegistrationForm above),
+    # Flask automatically runs these validators, in addition to any validators
+    # requested of those fields in validators=[...]
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('That username is already taken, please \
+                                    choose another.')
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+        if email is not None:
+            raise ValidationError('That email address is already in use.')
